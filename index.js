b1b627d7c0c5656e2bcbe765b0aa7eda46ab64a1
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2023-04-11 17:37:29 +0900

    anyway From Mac

--- index.js
+++ index.js
@@ -100,29 +100,38 @@ app.listen(port, "0.0.0.0", () => {
 // '/read_q_a'というGETのリクエストを受け取るエンドポイントで、q_aの全てのidとcontentとcreated_atとupdated_atとuserのnameを返す。contentはJSON.parseする
 app.get('/read_q_a', (req, res) => {
     const read_q_a = db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all();
-    console.log(read_q_a);
     res.send(read_q_a);
+});
+app.get('/read_f_i_b', (req, res) => {
+    const read_f_i_b = db.prepare('SELECT f_i_b.id, f_i_b.content, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all();
+    res.send(read_f_i_b);
 });
 
 // '/insert_q_a'というPOSTのリクエストを受け取るエンドポイントで、nameとpasswordを受け取り、nameとpasswordが一致する場合はそのユーザーのcontentとそのcontentのidとcreated_atとupdated_atを返す。sqlクエリの回数は2回までにする
 app.post('/insert_q_a', (req, res) => {
     const now = new Date().toISOString();
-    const [name, password, content] = [req.body.name, req.body.password, req.body.content];
-    const user = db.prepare('SELECT * FROM users WHERE name = ? AND password = ?').get(name, password);
-    if (user) {
-        const insert_q_a = db.prepare('INSERT INTO q_a (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(content), now, now);
-        // res.send(insert_q_a);
-// deleteが成功したら、q_aの全てのidとcontentとcreated_atとupdated_atとuserのnameを返す
-        if (insert_q_a.changes === 1) {
-            const read_q_a = db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all();
-            res.send(read_q_a);
-        } else {
-            res.send('q_aの追加に失敗しました');
-        };
-    } else {
-        res.send('ユーザーが存在しません');
-    }
+    const user = db.prepare('SELECT * FROM users WHERE name = ? AND password = ?').get(req.body.name, req.body.password);
+    user ? 
+        db.prepare('INSERT INTO q_a (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(req.body.content), now, now).changes === 1 ?
+            res.send(db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all())
+            :
+            res.send('q_aの追加に失敗しました')
+    :
+    res.send('ユーザーが存在しません');
 });
+// '/insert_f_i_b'というPOSTのリクエストを受け取るエンドポイントで、nameとpasswordを受け取り、nameとpasswordが一致する場合はそのユーザーのcontentとそのcontentのidとcreated_atとupdated_atを返す。sqlクエリの回数は2回までにする
+app.post('/insert_f_i_b', (req, res) => {
+    const now = new Date().toISOString();
+    const user = db.prepare('SELECT * FROM users WHERE name = ? AND password = ?').get(req.body.name, req.body.password);
+    user ?
+        db.prepare('INSERT INTO f_i_b (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(req.body.content), now, now).changes === 1 ?
+            res.send(db.prepare('SELECT f_i_b.id, f_i_b.content, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all())
+            :
+            res.send('f_i_bの追加に失敗しました')
+        :
+        res.send('ユーザーが存在しません');
+});
+
 
 // '/delete_q_a'というPOSTのリクエストを受け取るエンドポイントで、nameとpasswordを受け取り、nameとpasswordが一致する場合は、q_aのidが一致するものを削除する
 app.post('/delete_q_a', (req, res) => {
