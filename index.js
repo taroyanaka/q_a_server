9aa41d9abd9b885718a85910a103d01f4b53d9e9
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2023-04-18 22:42:40 +0900

    anyway From Mac

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // https://marketplace.visualstudio.com/items?itemName=emeraldwalk.RunOnSave
 
+
 // CCCCDEEEEEEEEEEE
 // in vscode's 
 // ./.vscode/settings.json
@@ -34,7 +35,32 @@
 
 
 
-// -- ユーザーのテーブル。カラムはIDはと名前とパスワードと作成日と更新日を持つ。IDは自動的に増加する
+// 上記のクライアント側の境界値テストのe2eテストのためのコード
+// '境界値テスト'のようなテストケースの類義語を10個
+
+// 以下に「境界値テスト」のようなテストケースの類義語を10個挙げます。
+
+// 端数処理テスト
+// 最大値テスト
+// 最小値テスト
+// 範囲テスト
+// 極端値テスト
+// 不正値テスト
+// 正常系テスト
+// 例外処理テスト
+// 網羅テスト
+// 省略テスト
+// 異常系テスト
+
+
+
+// DROP TABLE IF EXISTS users;
+// DROP TABLE IF EXISTS user_permission;
+// DROP TABLE IF EXISTS f_c;
+// DROP TABLE IF EXISTS f_i_b;
+// DROP TABLE IF EXISTS i_t_n;
+
+
 // CREATE TABLE users (
 //   id INTEGER PRIMARY KEY AUTOINCREMENT,
 //   role_id INTEGER NOT NULL,
@@ -43,48 +69,52 @@
 //   created_at DATETIME NOT NULL,
 //   updated_at DATETIME NOT NULL
 // );
-
-// -- ユーザーの権限のテーブル。カラムはIDはと名前と作成日と更新日を持つ。IDは自動的に増加する
-// -- カラムの中には、一般ユーザー、ゲストユーザーがある
-// -- ゲストユーザーはreadだけできる。一般ユーザーはread,write,deleteができる
-// CREATE TABLE roles (
+// CREATE TABLE user_permission (
 //   id INTEGER PRIMARY KEY,
-//   role TEXT NOT NULL,
-//   read INTEGER NOT NULL,
-//   write INTEGER NOT NULL,
-//   delete INTEGER NOT NULL, 
+
+//   permission TEXT NOT NULL,
+//   readable INTEGER NOT NULL,
+//   writable INTEGER NOT NULL,
+//   deletable INTEGER NOT NULL, 
+
 //   created_at DATETIME NOT NULL,
 //   updated_at DATETIME NOT NULL
 // );
 
-//  q_aというブログのようなサービスのテーブル。contentと作成日と更新日を持つ。IDは自動的に増加する。usersのIDを外部キーとして持つ
-// CREATE TABLE q_a (
+// CREATE TABLE f_c (
 //   id INTEGER PRIMARY KEY AUTOINCREMENT,
 //   user_id INTEGER NOT NULL,
-//   content TEXT NOT NULL,
+//   content_1 TEXT NOT NULL,
+//   content_2 TEXT NOT NULL,
 //   created_at DATETIME NOT NULL,
 //   updated_at DATETIME NOT NULL,
 //   FOREIGN KEY (user_id) REFERENCES users(id)
 // );
 
-//  f_i_bというブログのようなサービスのテーブル。contentと作成日と更新日を持つ。IDは自動的に増加する。usersのIDを外部キーとして持つ
 // CREATE TABLE f_i_b (
 //   id INTEGER PRIMARY KEY AUTOINCREMENT,
 //   user_id INTEGER NOT NULL,
-//   content TEXT NOT NULL,
+//   content_1 TEXT NOT NULL,
+//   content_2 TEXT NOT NULL,
 //   created_at DATETIME NOT NULL,
 //   updated_at DATETIME NOT NULL,
 //   FOREIGN KEY (user_id) REFERENCES users(id)
 // );
-//  i_t_nというブログのようなサービスのテーブル。contentと作成日と更新日を持つ。IDは自動的に増加する。usersのIDを外部キーとして持つ
 // CREATE TABLE i_t_n (
 //   id INTEGER PRIMARY KEY AUTOINCREMENT,
 //   user_id INTEGER NOT NULL,
-//   content TEXT NOT NULL,
+//   content_1 TEXT NOT NULL,
+//   content_2 TEXT NOT NULL,
 //   created_at DATETIME NOT NULL,
 //   updated_at DATETIME NOT NULL,
 //   FOREIGN KEY (user_id) REFERENCES users(id)
 // );
+
+// INSERT INTO users (role_id, name, password, created_at, updated_at) VALUES (1, 'PUBLIC', 'delete_24_hours', DATETIME('now'), DATETIME('now'));
+// INSERT INTO users (role_id, name, password, created_at, updated_at) VALUES (2, 'name1', 'password1', DATETIME('now'), DATETIME('now'));
+// INSERT INTO user_permission (id, permission, readable, writable, deletable, created_at, updated_at) VALUES (1, 'guest', 1, 0, 0, DATETIME('now'), DATETIME('now'));
+// INSERT INTO user_permission (id, permission, readable, writable, deletable, created_at, updated_at) VALUES (2, 'user', 1, 1, 1, DATETIME('now'), DATETIME('now'));
+
 
 
 
@@ -112,32 +142,6 @@ app.listen(port, "0.0.0.0", () => {
     console.log(`App listening!! at http://localhost:${port}`);
 });
 
-// read_i_t_n
-// insert_i_t_n
-// delete_i_t_n
-
-// '/read_q_a'というGETのリクエストを受け取るエンドポイントで、q_aの全てのidとcontentとcreated_atとupdated_atとuserのnameを返す。contentはJSON.parseする
-app.get('/read_q_a', (req, res) => {
-    const read_q_a = db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all();
-    res.send(read_q_a);
-});
-app.get('/read_f_i_b', (req, res) => {
-    const read_f_i_b = db.prepare('SELECT f_i_b.id, f_i_b.content, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all();
-    res.send(read_f_i_b);
-});
-app.get('/read_i_t_n', (req, res) => {
-    const read_i_t_n = db.prepare('SELECT i_t_n.id, i_t_n.content, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all();
-    res.send(read_i_t_n);
-});
-
-
-
-
-
-
-
-
-
 const now = () => new Date().toISOString();
 const user_with_permission = (REQ) => db.prepare('SELECT * FROM users INNER JOIN roles ON users.role_id = roles.id WHERE users.name = ? AND users.password = ?').get(REQ.body.name, REQ.body.password);
 // validator.jsでstringで4000文字以内のバリデーションをかける1行の関数
@@ -153,96 +157,75 @@ const true_if_within_4000_characters_and_not_empty = (str) => str.length > 0 && 
 // ja: 抽象化した関数を使って重複を削除する方針か、抽象化せずにコピーペーストする方針か、二択で、抽象化による罠を避けるために後者を選択した
 // en: The strategy is to either use an abstracted function to remove duplicates or to copy and paste without abstraction. The latter was selected to avoid traps due to abstraction.
 
-app.post('/insert_q_a', (req, res) => {
-    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content)) ? null : res.send('4000文字以内で入力して');
+// f_c
+// f_i_b
+// i_t_n
+
+// '/read_f_c'というGETのリクエストを受け取るエンドポイントで、f_cの全てのidとcontent1とcontent2とcreated_atとupdated_atとuserのnameを返す。contentはJSON.parseする
+app.get('/read_f_c', (req, res) => {
+    const rows = db.prepare('SELECT f_c.id, f_c.content_1, f_c.content_2, f_c.created_at, f_c.updated_at, users.name FROM f_c INNER JOIN users ON f_c.user_id = users.id').all();
+    res.json(rows);
+});
+// これは'/insert_f_c'というPOSTのリクエストを受け取るエンドポイントで、f_cにcontent1とcontent2を追加する。contentはJSON.stringifyする
+app.post('/insert_f_c', (req, res) => {
+    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content_1 + req.body_content_2)) ? null : res.send('4000文字以内で入力して');
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.writable === 1 ? null : res.send('書き込み権限がありません');
-    db.prepare('INSERT INTO q_a (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(req.body.content), now(), now()).changes === 1
-        ? res.send(db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all()) : res.send('q_aの追加に失敗しました');
+    db.prepare('INSERT INTO f_c (user_id, content_1, content_2, created_at, updated_at) VALUES (?, ?, ?, ?, ?)').run(user.id, req.body.content_1, req.body.content_2, now(), now()).changes === 1
+        ? res.send(db.prepare('SELECT f_c.id, f_c.content_1, f_c.content_2, f_c.created_at, f_c.updated_at, users.name FROM f_c INNER JOIN users ON f_c.user_id = users.id').all()) : res.send('f_cの追加に失敗しました');
 });
-app.post('/delete_q_a', (req, res) => {
+// これは'/delete_f_c'というPOSTのリクエストを受け取るエンドポイントで、f_cのidを指定して削除する
+app.post('/delete_f_c', (req, res) => {
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.deletable === 1 ? null : res.send('削除権限がありません');
-    db.prepare('DELETE FROM q_a WHERE id = ?').run(req.body.id).changes === 1
-        ? res.send(db.prepare('SELECT q_a.id, q_a.content, q_a.created_at, q_a.updated_at, users.name FROM q_a INNER JOIN users ON q_a.user_id = users.id').all()) : res.send('q_aの削除に失敗しました');
+    db.prepare('DELETE FROM f_c WHERE id = ?').run(req.body.id).changes === 1
+        ? res.send(db.prepare('SELECT f_c.id, f_c.content_1, f_c.content_2, f_c.created_at, f_c.updated_at, users.name FROM f_c INNER JOIN users ON f_c.user_id = users.id').all()) : res.send('f_cの削除に失敗しました');
 });
 
+// '/read_f_i_b'というGETのリクエストを受け取るエンドポイントで、f_i_bの全てのidとcontent1とcontent2とcreated_atとupdated_atとuserのnameを返す。contentはJSON.parseする
+app.get('/read_f_i_b', (req, res) => {
+    const rows = db.prepare('SELECT f_i_b.id, f_i_b.content_1, f_i_b.content_2, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all();
+    res.json(rows);
+});
+// これは'/insert_f_i_b'というPOSTのリクエストを受け取るエンドポイントで、f_i_bにcontent1とcontent2を追加する。contentはJSON.stringifyする
 app.post('/insert_f_i_b', (req, res) => {
-    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content)) ? null : res.send('4000文字以内で入力して');
+    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content_1 + req.body_content_2)) ? null : res.send('4000文字以内で入力して');
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.writable === 1 ? null : res.send('書き込み権限がありません');
-    db.prepare('INSERT INTO f_i_b (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(req.body.content), now(), now()).changes === 1
-        ? res.send(db.prepare('SELECT f_i_b.id, f_i_b.content, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all()) : res.send('f_i_bの追加に失敗しました');
+    db.prepare('INSERT INTO f_i_b (user_id, content_1, content_2, created_at, updated_at) VALUES (?, ?, ?, ?, ?)').run(user.id, req.body.content_1, req.body.content_2, now(), now()).changes === 1
+        ? res.send(db.prepare('SELECT f_i_b.id, f_i_b.content_1, f_i_b.content_2, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all()) : res.send('f_i_bの追加に失敗しました');
 });
+// これは'/delete_f_i_b'というPOSTのリクエストを受け取るエンドポイントで、f_i_bのidを指定して削除する
 app.post('/delete_f_i_b', (req, res) => {
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.deletable === 1 ? null : res.send('削除権限がありません');
     db.prepare('DELETE FROM f_i_b WHERE id = ?').run(req.body.id).changes === 1
-        ? res.send(db.prepare('SELECT f_i_b.id, f_i_b.content, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all()) : res.send('f_i_bの削除に失敗しました');
+        ? res.send(db.prepare('SELECT f_i_b.id, f_i_b.content_1, f_i_b.content_2, f_i_b.created_at, f_i_b.updated_at, users.name FROM f_i_b INNER JOIN users ON f_i_b.user_id = users.id').all()) : res.send('f_i_bの削除に失敗しました');
 });
 
+// '/read_i_t_n'というGETのリクエストを受け取るエンドポイントで、i_t_nの全てのidとcontent1とcontent2とcreated_atとupdated_atとuserのnameを返す。contentはJSON.parseする
+app.get('/read_i_t_n', (req, res) => {
+    const rows = db.prepare('SELECT i_t_n.id, i_t_n.content_1, i_t_n.content_2, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all();
+    res.json(rows);
+});
+// これは'/insert_i_t_n'というPOSTのリクエストを受け取るエンドポイントで、i_t_nにcontent1とcontent2を追加する。contentはJSON.stringifyする
 app.post('/insert_i_t_n', (req, res) => {
-    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content)) ? null : res.send('4000文字以内で入力して');
+    true_if_within_4000_characters_and_not_empty(JSON.stringify(req.body.content_1 + req.body_content_2)) ? null : res.send('4000文字以内で入力して');
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.writable === 1 ? null : res.send('書き込み権限がありません');
-    db.prepare('INSERT INTO i_t_n (user_id, content, created_at, updated_at) VALUES (?, ?, ?, ?)').run(user.id, JSON.stringify(req.body.content), now(), now()).changes === 1
-        ? res.send(db.prepare('SELECT i_t_n.id, i_t_n.content, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all()) : res.send('i_t_nの追加に失敗しました');
+    db.prepare('INSERT INTO i_t_n (user_id, content_1, content_2, created_at, updated_at) VALUES (?, ?, ?, ?, ?)').run(user.id, req.body.content_1, req.body.content_2, now(), now()).changes === 1
+        ? res.send(db.prepare('SELECT i_t_n.id, i_t_n.content_1, i_t_n.content_2, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all()) : res.send('i_t_nの追加に失敗しました');
 });
+// これは'/delete_i_t_n'というPOSTのリクエストを受け取るエンドポイントで、i_t_nのidを指定して削除する
 app.post('/delete_i_t_n', (req, res) => {
     const user = user_with_permission(req);
     user ? null : res.send('ユーザーが存在しません');
     user.deletable === 1 ? null : res.send('削除権限がありません');
     db.prepare('DELETE FROM i_t_n WHERE id = ?').run(req.body.id).changes === 1
-        ? res.send(db.prepare('SELECT i_t_n.id, i_t_n.content, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all()) : res.send('i_t_nの削除に失敗しました');
+        ? res.send(db.prepare('SELECT i_t_n.id, i_t_n.content_1, i_t_n.content_2, i_t_n.created_at, i_t_n.updated_at, users.name FROM i_t_n INNER JOIN users ON i_t_n.user_id = users.id').all()) : res.send('i_t_nの削除に失敗しました');
 });
-
-
-
-// 上記のクライアント側の境界値テストのe2eテストのためのコード
-// fetch('http://localhost:3000/insert_q_a', {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     credentials: 'include',
-//     body: JSON.stringify({
-//         content: {
-//             question: 'a'.repeat(4000),
-//             answer: 'a'.repeat(4000)
-//         }
-//     })
-// }).then(response => response.text()).then(data => console.log(data));
-// fetch('http://localhost:3000/insert_f_i_b', {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     credentials: 'include',
-//     body: JSON.stringify({
-//         content: {
-//             question: 'a'.repeat(4000),
-//             answer: 'a'.repeat(4000)
-//         }
-//     })
-// }).then(response => response.text()).then(data => console.log(data));
-// fetch('http://localhost:3000/insert_i_t_n', {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     credentials: 'include',
-//     body: JSON.stringify({
-//         content: {
-//             question: 'a'.repeat(4000),
-//             answer: 'a'.repeat(4000)
-//         }
-//     })
-// }).then(response => response.text()).then(data => console.log(data));
